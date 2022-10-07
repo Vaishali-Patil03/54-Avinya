1d4fd640e9d727199f92832854182ba34c529f80
Abhishek Prajapati <91844013+abhishekce17@users.noreply.github.com>
2022-10-07 22:57:52 +0530

    Update serviceScanner.py

--- serviceScanner.py
+++ serviceScanner.py
@@ -6,11 +6,14 @@ def Running_processes():
     for process in f.Win32_Process():
         print(f"{process.ProcessId:<10} {process.Name}")
 
+
 def List_services():
     f = wmi.WMI()
     print("pid        Service name                                            Status")
     for service in f.Win32_Service():
-        print(f"{service.ProcessId:<10}    {service.Caption:<10}              {service.State:} ")
+        print(
+            f"{service.ProcessId:<10}    {service.Caption:<10}              {service.State:} ")
+
 
 def terminate_processes(name):
     ti = 0
@@ -23,23 +26,20 @@ def terminate_processes(name):
                 ti += 1
     if ti == 0:
         print("Process not found")
-    
-def terminate_services(service_name):
-    pass
 
 if __name__ == "__main__":
     while True:
         print("1. Show all running Processes \n2. Show all running services \n3. Stop Specific Process \n4. Quit")
         option = int(input("Write Option number : "))
         if option == 1:
-            Running_processes() 
+            Running_processes()  # for showing running processes
         elif option == 2:
-            List_services()    
+            List_services()  # for listing services
         elif option == 3:
             process_name = input("write process name to stop process : ")
             terminate_processes(process_name)
         elif option == 4:
             break
         iteration = input("do you want to continue (y/n) : ")
-        if(iteration == "n"):
+        if (iteration == "n"):
             break
